0c5e49d111ca1ad7d8fec733ba79ae74869fe536
wguilbert <williamguilbert@hotmail.com>
2024-09-10 18:45:10 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
 function App() {
+  const [cities, setCities] = useState([]);
+
+  useEffect(function () {
+    async function getCities() {
+      const res = await fetch(
+        `http://localhost:${process.env.REACT_APP_API_PORT}/city`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await res.json();
+      setCities(data);
+    }
+    getCities();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {cities.map((city) => (
+        <>
+          <p>{city.city}</p>
+          <p>{city.country}</p>
+        </>
+      ))}
     </div>
   );
 }
